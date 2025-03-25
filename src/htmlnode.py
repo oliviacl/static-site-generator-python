36a37e594bc6aa7f222109705617b8044a166921
Olivia C. <olivialngc1@gmail.com>
2025-03-25 07:56:10 +0100

    feat: add ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,3 +32,27 @@ class LeafNode(HTMLNode):
             return self.value
 
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid html: all parent nodes must have a tag")
+
+        if self.children is None:
+            raise ValueError("invalid html: all parent nodes must have children")
+
+        html = f"<{self.tag}{self.props_to_html()}>"
+
+        for child in self.children:
+            html += child.to_html()
+
+        html += f"</{self.tag}>"
+
+        return html
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
